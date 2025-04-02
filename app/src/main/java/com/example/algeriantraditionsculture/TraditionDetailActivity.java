ec12be1c6d69c2e0aa29ec3107eb4dfc9ac457d4
chakib <chakib12467@gmail.com>
2025-04-02 01:19:14 +0100

    version 2 (working)

--- app/src/main/java/com/example/algeriantraditionsculture/TraditionDetailActivity.java
+++ app/src/main/java/com/example/algeriantraditionsculture/TraditionDetailActivity.java
@@ -5,6 +5,7 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
